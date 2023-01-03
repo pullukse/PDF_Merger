75da24ed64a7145a48212e836fddb5552c428f80
Seray Pulluk <81200605+pullukse@users.noreply.github.com>
2023-01-02 21:03:25 -0500

    Update PDF_merger.py

--- PDF_merger.py
+++ PDF_merger.py
@@ -1,17 +1,27 @@
 #All files have to be in PDF format and located under the same folder.
 
+import os
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 def merge_pdfs(paths, output):
-    pdf_writer = PdfFileWriter()
+    pdf_writer = PdfWriter()
+    
     for path in paths:
-        pdf_reader = PdfFileReader(path)
-    for page in range (pdf_reader.getNumPages()) :
-        # Add each page to the writer object
-        pdf_writer.addPage(pdf_reader.getPage(page))
-        # Write out the merged PDF
+        pdf_reader = PdfReader(path)
+        for page in range(len(pdf_reader.pages)):
+            # Add each page to the writer object
+            pdf_writer.add_page(pdf_reader.pages[page])
+    
+    # Write out the merged PDF
     with open(output,'wb') as out:
-        pdf_writer.write (out)
+        pdf_writer.write(out)
+        
+        
+cwd = os.getcwd()
+files = os.listdir(cwd)
+
+#Ensure PDFs are in your current directory
+files 
         
 if __name__ == '__main__':
     paths = [#insert str (with .pdf) of the files to be merged by desired order of pages]
